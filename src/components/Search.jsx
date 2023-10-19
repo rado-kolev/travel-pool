49290180d8eaf4633008121767bd9fad37539a54
rado-kolev <rado.k.kolev@gmail.com>
2023-10-19 19:36:49 +0100

    Select options change

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,11 +13,10 @@ const Search = () => {
             set along the most gorgeous tropical settings and exquisite beaches
             in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados and
             Curaçao, feature unlimited gourmet dining, unique bars serving
-            premium liquors and wines, and every land and water sport,
-            including complimentary green fees at our golf resorts and certified
-            scuba diving at most resorts. If you are planning a wedding, BEACHES
-            is the leader in Caribbean destination weddings and honeymoon
-            packages.
+            premium liquors and wines, and every land and water sport, including
+            complimentary green fees at our golf resorts and certified scuba
+            diving at most resorts. If you are planning a wedding, BEACHES is
+            the leader in Caribbean destination weddings and honeymoon packages.
           </p>
         </div>
         <div className='grid sm:grid-cols-2 gap-8 py-4'>
@@ -54,11 +53,16 @@ const Search = () => {
         <form className='w-full'>
           <div className='flex flex-col my-2'>
             <label>Destination</label>
-            <select className='border rounded-md p-2'>
-              <option>Grande Antigua</option>
-              <option>Key West</option>
-              <option>Maldives</option>
-              <option>Cozumel</option>
+            <select className='border rounded-md p-2 cursor-pointer'>
+              <option selected>
+                Choose Your Destination
+              </option>
+              <option>Antigua</option>
+              <option>Curacao</option>
+              <option>Grenada</option>
+              <option>Jamaica</option>
+              <option>Saint Lucia</option>
+              <option>The Bahamas</option>
             </select>
           </div>
           <div className='flex flex-col my-4'>
